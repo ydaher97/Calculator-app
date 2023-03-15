e82985946f6c83f03c73ad2436c587484d4ad28a
Emre Turan <itsemreturan@gmail.com>
2023-03-15 18:39:47 +0300

    code formatted

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-import { Calculator } from './calculator.js';
+import { Calculator } from "./calculator.js";
 let operator = 0;
 let digit = 0;
 
-const calculatorDisplay = document.getElementById('calculator-display');
+const calculatorDisplay = document.getElementById("calculator-display");
 const calculator = new Calculator(calculatorDisplay);
 
-const numberButtons = document.querySelectorAll('.number');
-const operatorButtons = document.querySelectorAll('.operator');
-const clearButton = document.querySelector('.clear');
-const equalsButton = document.querySelector('.equals');
-const backButton = document.querySelector('.back');
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const clearButton = document.querySelector(".clear");
+const equalsButton = document.querySelector(".equals");
+const backButton = document.querySelector(".back");
 
-numberButtons.forEach(button => {
-  button.addEventListener('click', () => {
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
     calculator.addToInput(button.value);
     digit++;
     console.log("button");
   });
 });
 
-operatorButtons.forEach(button => {
-  button.addEventListener('click', () => {
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", () => {
     if (digit > 0 && operator < 1) {
       calculator.addOperator(button.value);
       calculator.calculateLongExpression();
@@ -30,24 +30,24 @@ operatorButtons.forEach(button => {
   });
 });
 
-clearButton.addEventListener('click', () => {
+clearButton.addEventListener("click", () => {
   digit = 0;
   operator = 0;
   calculator.clear();
 });
 
-equalsButton.addEventListener('click', () => {
+equalsButton.addEventListener("click", () => {
   calculator.calculate();
   operator = 0;
   digit = 1;
 });
 
-backButton.addEventListener('click', () => {
+backButton.addEventListener("click", () => {
   const display = calculatorDisplay.value;
-  if ((display[display.length-1]).match(/\d/)) {
+  if (display[display.length - 1].match(/\d/)) {
     digit--;
   } else {
     operator = 0;
   }
   calculator.back();
-  });
+});
