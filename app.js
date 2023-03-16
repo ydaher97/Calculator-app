b15fd53f85587639b5a18f9415de452863572409
VladislavKostadinov <vladicius@gmail.com>
2023-03-16 13:42:39 +0200

    Added floating numbers logic
    Added decimal sign functionality.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import { Calculator } from "./calculator.js";
 let operator = 0;
 let digit = 0;
+let decimal = 0;
 
 const calculatorDisplay = document.getElementById("calculator-display");
 const calculator = new Calculator(calculatorDisplay);
@@ -10,6 +11,7 @@ const operatorButtons = document.querySelectorAll(".operator");
 const clearButton = document.querySelector(".clear");
 const equalsButton = document.querySelector(".equals");
 const backButton = document.querySelector(".back");
+const decimalButton = document.querySelector(".decimal");
 
 numberButtons.forEach((button) => {
   button.addEventListener("click", () => {
@@ -19,16 +21,25 @@ numberButtons.forEach((button) => {
   });
 });
 
+decimalButton.addEventListener("click", () => {
+  if (digit > 0 && decimal == 0) {
+  calculator.addToInput('.');
+  decimal = 1;
+}
+});
+
 operatorButtons.forEach((button) => {
   button.addEventListener("click", () => {
     if (operator === 0 && digit === 1) {
       calculator.addOperator(button.value);
       operator = 1;
       digit = 0;
+      decimal = 0;
     } else if (operator === 1 && digit === 1) {
       calculator.calculate();
       calculator.addOperator(button.value);
       digit = 0;
+      decimal = 0;
     } else if (operator === 1 && digit === 0) {
       calculator.back();
       calculator.addOperator(button.value);
@@ -39,6 +50,7 @@ operatorButtons.forEach((button) => {
 clearButton.addEventListener("click", () => {
   digit = 0;
   operator = 0;
+  decimal = 0;
   calculator.clear();
 });
 
@@ -47,6 +59,7 @@ equalsButton.addEventListener("click", () => {
     calculator.calculate();
     operator = 0;
     digit = 1;
+    decimal = 0;
   }
 });
 
@@ -59,9 +72,11 @@ backButton.addEventListener("click", () => {
     } else {
       digit = 0;
       operator = 0;
+      decimal = 0;
     }
   } else {
     digit = 0;
     operator = 0;
+    decimal = 0;
   }
 });
